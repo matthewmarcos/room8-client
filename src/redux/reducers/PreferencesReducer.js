6fa2ca8e946200c2ffebafdfb9289c74ee7cdaf8
Matthew Marcos <matthewmarcos94@gmail.com>
2017-04-05 15:52:17 +0800

    Create new starting reducer state for user

--- src/redux/reducers/PreferencesReducer.js
+++ src/redux/reducers/PreferencesReducer.js
@@ -1,7 +1,50 @@
 const preferencesReducerInitialState = {
-    wantedCleanliness: 10
-};
+    wantedCleanliness: 10,
+    startDate: new Date().toISOString(),
+    duration: 'Indefinitely',
 
+    //Cost
+    rentPriceRangeStart: 1000,
+    rentPriceRangeEnd: 5000,
+    utilities: [
+        ''
+    ]
+
+
+};
+    //  {/* When */}
+    //             {/* Start Date - EditDate */}
+    //             {/* Duration - EditDropdown */}
+
+    //         {/* Cost */}
+    //             {/* Rent Price Range */}
+    //             {/* I want my rent to include utilities - EditDropdown */}
+    //             {/* Utilities Price Range */}
+
+    //         {/* Location */}
+    //             {/* I want to be a 5 minute walking distance from restaurants */}
+    //             {/* I want to be within x minutes to UPLB */}
+    //             {/* I want to be within the general area of: */}
+
+
+    //         {/* Utilities */}
+    //             {/* I want to do my own laundry */}
+    //             {/* I want to be able to cook */}
+    //             {/* Checkbox of Cooking Utilities */}
+    //             {/* I want Air Conditioning */}
+    //             {/* I want Internet */}
+    //             {/* Minimum and Maximum speed */}
+
+    //         {/* Lifestyle */}
+    //             {/* I am okay with Alcohol */}
+    //             {/* I am okay with Smokers */}
+    //             {/* I am okay with roommates affiliated with an org */}
+    //             {/* I want to bring guests to the room /}
+    //             {/* I need a study area for guests */}
+    //             {/* I study best in the */}
+    //             {/* I have pets */}
+    //                 {/* Small Caged Pets (ex. Hamsters) */}
+    //                 {/* Dogs/Cats */}
 export const preferencesReducer = (state = preferencesReducerInitialState, action) => {
     switch (action.type) {
         case 'ACTION_TYpE_1': {
--- src/redux/reducers/UserReducer.js
+++ src/redux/reducers/UserReducer.js
@@ -1,60 +1,52 @@
-const userReducerInitialState = {
-    userId: '1234',
-    isLoggedIn: true,
-    username: 'mattemhotep',
+// const userReducerInitialState = {
+//     userId: '1234',
+//     isLoggedIn: true,
+//     username: 'mattemhotep',
 
 
-    /* User Profile */
-    fullName: 'Jonathan Crow-Secant',
-    nickname: 'Joe',
-    status: 'I am looking for a room',
-    contactNumber: '1234567',
-    email: 'joe12@gmail.com',
-    cleanliness: 5,
-    sex: 'Male',
-    gender: 'Male',
-    course: 'BS Computer Science',
-    batch: '2013',
-    organizations: ['YSES', 'Jammers', 'Painters'],
-    hobbies: ['guitar', 'drums', 'saxophone', 'dancing'],
-    interests: ['BananaMaking', 'zumba'],
-    bio: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-    birthday: new Date()
-};
+//     /* User Profile */
+//     fullName: 'Jonathan Crow-Secant',
+//     nickname: 'Joe',
+//     status: 'I am looking for a room',
+//     contactNumber: '1234567',
+//     email: 'joe12@gmail.com',
+//     cleanliness: 5,
+//     sex: 'Male',
+//     gender: 'Male',
+//     course: 'BS Computer Science',
+//     batch: '2013',
+//     organizations: ['YSES', 'Jammers', 'Painters'],
+//     hobbies: ['guitar', 'drums', 'saxophone', 'dancing'],
+//     interests: ['BananaMaking', 'zumba'],
+//     bio: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+//     birthday: new Date()
+// };
 
 
 // On app start, set the tempFields to what is fetched from server.
+const userReducerInitialState = {
+    userId: '',
+    isLoggedIn: false,
+    username: '',
 
-// const userReducerInitialState = {
-    // isLoggedIn: false,
-    // userId: '',
-    // status: 'I am looking for a room',
-    // username: '',
-    // fullName: '',
-    // contactNumber: '',
-    // sex: 'ApacheHelicopter',
-    // gender: 'ApacheHelicopter',
-    // course: '',
-    // batch:'',
-    // birthDate: null,
-    // organizations: [],
-    // hobbies: [],
-    // interests: [],
-    // bio: '',
-//
-    // tempFields: {
-        // username: '',
-        // bio: '',
-        // status: '',
-        // fullName: '',
-        // userId: '',
-        // contactNumber: '',
-        // sex: '',
-        // gender: '',
-        // course: '',
-        // batch: ''
-    // }
-// };
+    /* User Profile */
+    fullName: '',
+    nickname: '',
+    status: '',
+    contactNumber: '',
+    email: '',
+    cleanliness: 5,
+    sex: '',
+    gender: '',
+    course: '',
+    batch: '',
+    organizations: [],
+    hobbies: [],
+    interests: [],
+    bio: '',
+    birthday: null
+};
+
 
 export const userReducer = (state = userReducerInitialState, action) => {
 
