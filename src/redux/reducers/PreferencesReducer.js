97b46969e232e15fbb19a9343cc8e5c2ee208f5e
Matthew Marcos <matthewmarcos94@gmail.com>
2017-04-14 16:20:06 +0800

    Add initial state for preferences Reducer

--- src/redux/reducers/PreferencesReducer.js
+++ src/redux/reducers/PreferencesReducer.js
@@ -1,55 +1,59 @@
 const preferencesReducerInitialState = {
-    wantedCleanliness: 10,
-    startDate: new Date().toISOString(),
-    duration: 'Indefinitely',
-
-    //Cost
-    rentPriceRangeStart: 1000,
-    rentPriceRangeEnd: 5000,
-    utilities: [
-        ''
-    ]
+    airconditioning: null,
+    alcohol: null,
+    cleanliness: null,
+    cooking: null,
+    curfew: null,
+    curfewTime: null,
+    duration: null,
+    electricStove: null,
+    gasStove: null,
+    generalLocation: null,
+    guestsInRoom: null,
+    guestsStudyArea: null,
+    internet: null,
+    laundry: null,
+    message: null,
+    microwave: null,
+    nearbyRestaurants: null,
+    org: null,
+    pets: null,
+    rentPriceRangeEnd: null,
+    rentPriceRangeStart: null,
+    sex: null,
+    shouldIncludeUtilities: null,
+    smokers: null,
+    speedRequirement: null,
+    startDate: null,
+    studyTime: null,
+    torrent: null,
+    travelTimeToUplb: null,
+    utilitiesPriceRangeEnd: null,
+    utilitiesPriceRangeStart: null,
+    waterKettle: null,
 };
-    //  {/* When */}
-    //             {/* Start Date - EditDate */}
-    //             {/* Duration - EditDropdown */}
-
-    //         {/* Cost */}
-    //             {/* Rent Price Range */}
-    //             {/* I want my rent to include utilities - EditDropdown */}
-    //             {/* Utilities Price Range */}
-
-    //         {/* Location */}
-    //             {/* I want to be a 5 minute walking distance from restaurants */}
-    //             {/* I want to be within x minutes to UPLB */}
-    //             {/* I want to be within the general area of: */}
 
 
-    //         {/* Utilities */}
-    //             {/* I want to do my own laundry */}
-    //             {/* I want to be able to cook */}
-    //             {/* Checkbox of Cooking Utilities */}
-    //             {/* I want Air Conditioning */}
-    //             {/* I want Internet */}
-    //             {/* Minimum and Maximum speed */}
-
-    //         {/* Lifestyle */}
-    //             {/* I am okay with Alcohol */}
-    //             {/* I am okay with Smokers */}
-    //             {/* I am okay with roommates affiliated with an org */}
-    //             {/* I want to bring guests to the room /}
-    //             {/* I need a study area for guests */}
-    //             {/* I study best in the */}
-    //             {/* I have pets */}
-    //                 {/* Small Caged Pets (ex. Hamsters) */}
-    //                 {/* Dogs/Cats */}
 export const preferencesReducer = (state = preferencesReducerInitialState, action) => {
     switch (action.type) {
         case 'RECEIVED_PREFERENCES_DATA': {
             const { payload } = action;
+            let toInsert = {
+                ...payload.preferences
+            };
+
+            // Getting rid of excess data from the big query
+            delete toInsert.username;
+            delete toInsert.id;
+            delete toInsert.email;
+            delete toInsert.nickname;
+            delete toInsert.timeRegistered;
+
+            console.log(toInsert);
+
             return {
                 ...state,
-                ...payload.preferences
+                ...toInsert
             };
         }
 
