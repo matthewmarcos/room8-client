a8ad0f3944da376ad14959e1713e4c1852e14de3
Matthew Marcos <matthewmarcos94@gmail.com>
2017-04-18 04:17:23 +0800

    Redirect to login after successful register

--- src/redux/reducers/UserReducer.js
+++ src/redux/reducers/UserReducer.js
@@ -1,28 +1,5 @@
 import _ from 'lodash';
-// const userReducerInitialState = {
-//     userId: '1234',
-//     isLoggedIn: true,
-//     username: 'mattemhotep',
-
-
-//     /* User Profile */
-//     fullName: 'Jonathan Crow-Secant',
-//     nickname: 'Joe',
-//     status: 'I am looking for a room',
-//     contactNumber: '1234567',
-//     email: 'joe12@gmail.com',
-//     cleanliness: 5,
-//     sex: 'Male',
-//     gender: 'Male',
-//     course: 'BS Computer Science',
-//     batch: '2013',
-//     organizations: ['YSES', 'Jammers', 'Painters'],
-//     hobbies: ['guitar', 'drums', 'saxophone', 'dancing'],
-//     interests: ['BananaMaking', 'zumba'],
-//     bio: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-//     birthday: new Date()
-// };
-
+import { browserHistory } from 'react-router';
 
 // On app start, set the tempFields to what is fetched from server.
 export const userReducerInitialState = {
@@ -177,6 +154,17 @@ export const userReducer = (state = userReducerInitialState, action) => {
                 ]
             };
         }
+
+
+        case 'REGISTER_FULFILLED': {
+            const { payload } = action;
+
+            browserHistory.push('/login');
+            return {
+                ...state,
+            };
+        }
+
 
 
         default: {
