c81fa3c57354aeabbdb350847a37332ee2bad3f2
Matthew Marcos <matthewmarcos94@gmail.com>
2017-05-16 06:02:08 +0800

    Remove notifications list

--- src/components/AuthedNavbar/AuthedNavbar.jsx
+++ src/components/AuthedNavbar/AuthedNavbar.jsx
@@ -39,7 +39,6 @@ const AuthedNavbar = (props) => {
                     </span>
                 </NavItem>
             </LinkContainer>
-            <NotificationsList {...props}/>
             <DropdownMenu />
        </Nav>
     );
